734539e2b09c98811d339e1a3f634836f3ae1082
Domenico Benfenati <domy.benf@gmail.com>
2022-06-26 12:30:41 +0200

    Add files via upload

---
+++ Python Quiz/data.py
@@ -0,0 +1,30 @@
+quiz_data = [
+    {
+        "question": "qual è lo stato più piccolo d'Europa?",
+        "correct_answer": "vaticano",
+    },
+    {
+        "question": "quante volte è stato presidente Giulio Andreotti?",
+        "correct_answer": "7",
+    },
+    {
+        "question": "quale mare tra Tirreno, Ionio, Mediterraneo e Morto non bagna l'Italia?",
+        "correct_answer": "Morto",
+    },
+    {
+        "question": "a quando risale l'invenzione dello champagne da parte di Don Perignon?",
+        "correct_answer": "1668",
+    },
+    {
+        "question": "chi è l'autore del cantico delle creature?",
+        "correct_answer": "San Francesco",
+    },
+    {
+        "question": "qual è lo strato dell'atmosfera che si trova più in basso?",
+        "correct_answer": "troposfera",
+    },
+    {
+        "question": "dove si svolgerà il campionato del mondo di calcio 2022?",
+        "correct_answer": "Qatar",
+    }
+]
